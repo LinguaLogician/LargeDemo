e5b4c206f779ae1fd749bbf18addef3da64822cf
Liangtao <wlt1990@outlook.com>
2025-08-27 16:43:35 +0800

    change directory

--- BERT/BertWithPretrained/model/DownstreamTasks/__init__.py
+++
@@ -1,7 +0,0 @@
-from .BertForSentenceClassification import BertForSentenceClassification
-from .BertForMultipleChoice import BertForMultipleChoice
-from .BertForQuestionAnswering import BertForQuestionAnswering
-from .BertForNSPAndMLM import BertForNextSentencePrediction
-from .BertForNSPAndMLM import BertForMaskedLM
-from .BertForNSPAndMLM import BertForPretrainingModel
-from .BertForTokenClassification import BertForTokenClassification
